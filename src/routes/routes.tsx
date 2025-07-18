be0bdbc4de6a80044ee8a979a9a8d4358a16385c
Rubul-Tanti <rubultanti404@gmail.com>
2025-07-18 22:35:56 +0530

    added sales page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 
 import { Route,Routes } from 'react-router-dom'
 import Login from '../pages/login'
+import Sales from '../pages/sales'
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path='/login' element={<Login/>}></Route>
+        <Route path='/sales' element={<Sales/>}></Route>
       </Routes>
     </div>
   )
